aced9786c05ad59cb8fa08a837fadf2286d7fdbd
Abhay Sarda <asarda@bu.edu>
2018-10-21 02:15:12 -0400

    Created server backend

---
+++ README.md
@@ -0,0 +1,29 @@
+# Getting started
+
+DoTA 2 client for ILP running on Heroku and Wix. This repository contains all the Heroku code and the client side script, pay.js.
+
+
+# Want to play?
+
+# Developer section
+
+## Running Locally
+
+Make sure you have [Node.js](http://nodejs.org/) and the [Heroku CLI](https://cli.heroku.com/) installed.
+
+```sh
+$ git clone https://github.com/abhaysarda/pay2play.git # or clone your own fork
+$ cd pay2play
+$ npm install
+$ npm start
+```
+
+Your app should now be running on [localhost:5000](http://localhost:5000/).
+
+## Deploying to Heroku
+
+```
+$ heroku create pay2play
+$ git push heroku master
+$ heroku open
+```
---
+++ index.js
@@ -0,0 +1,66 @@
+const http = require('http')
+const url = require('url')
+const crypto = require('crypto')
+const plugin = require('./plugins.js').xrp.Wix()
+
+
+const cost = 1000
+
+function base64url (buf) {
+  return buf.toString('base64')
+    .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
+}
+
+
+
+    console.log(` 1. Connecting to an account to accept payments...`)
+
+
+    plugin.connect().then(function () {
+      // Get ledger and account information from the plugin
+      const ledgerInfo = plugin.getInfo()
+      const account = plugin.getAccount()
+
+      console.log(`    - Connected to ledger: ${ledgerInfo.prefix}`)
+      console.log(`    -- Account: ${account}`)
+      console.log(`    -- Currency: ${ledgerInfo.currencyCode}`)
+      console.log(`    -- CurrencyScale: ${ledgerInfo.currencyScale}`)
+
+      // Convert our cost (1000) into the right format given the ledger scale
+      const normalizedCost = cost / Math.pow(10, parseInt(ledgerInfo.currencyScale))
+
+      console.log(`    - Charging ${normalizedCost} ${ledgerInfo.currencyCode}`)
+
+    });
+
+
+      // Handle incoming payments
+    plugin.on('incoming_prepare', function (transfer) {
+      if (parseInt(transfer.amount) < 1000) {
+        // Transfer amount is incorrect
+        console.log(`    - Payment received for the wrong amount ` +
+                                          `(${transfer.amount})... Rejected`)
+
+        const normalizedAmount = transfer.amount /
+                              Math.pow(10, parseInt(ledgerInfo.currencyScale))
+
+        plugin.rejectIncomingTransfer(transfer.id, {
+          code: 'F04',
+          name: 'Insufficient Destination Amount',
+          message: `Please send at least 10 ${ledgerInfo.currencyCode},` +
+                    `you sent ${normalizedAmount}`,
+          triggered_by: plugin.getAccount(),
+          triggered_at: new Date().toISOString(),
+          forwarded_by: [],
+          additional_info: {}
+        })
+      } else {
+
+        console.log(`Payment complete! ${transfer.from} bid ${transfer.amount}`);
+      }
+    })
+
+
+//wixSetUp().then(console.log)
+
+//module.exports = wixSetUp;
